e68a39ee64d84962b58088c5deefded1653d7fb5
tlugat <teo.lugat@ausha.co>
2024-06-19 20:34:12 +0200

    ✨ event websocket

---
+++ lib/services/event_websocket_service.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import '../models/event.dart';
+import '../models/user.dart';
+import '../models/expense.dart';
+import '../services/websocket_service.dart';
+
+class EventWebsocketProvider with ChangeNotifier {
+  final WebSocketService _webSocketService;
+  Event? _event;
+  List<User> _users = [];
+  List<Expense> _expenses = [];
+
+  Event? get event => _event;
+  List<User> get users => _users;
+  List<Expense> get expenses => _expenses;
+
+  EventWebsocketProvider(this._webSocketService) {
+    _listenToWebSocket();
+  }
+
+  void _listenToWebSocket() {
+    _webSocketService.stream?.listen((message) {
+      final data = jsonDecode(message);
+      final type = data['type'];
+      final payload = data['payload'];
+
+      switch (type) {
+        case 'event':
+          final event = Event.fromJson(payload);
+          _updateEvent(event);
+          break;
+        case 'expenses':
+          final expenses =
+              (payload as List).map((e) => Expense.fromJson(e)).toList();
+          _updateExpenses(expenses);
+          break;
+        case 'users':
+          final users = (payload as List).map((u) => User.fromJson(u)).toList();
+          _updateUsers(users);
+          break;
+      }
+    });
+  }
+
+  void _updateEvent(Event event) {
+    _event = event;
+    notifyListeners();
+  }
+
+  void _updateExpenses(List<Expense> expenses) {
+    _expenses = expenses;
+    notifyListeners();
+  }
+
+  void _updateUsers(List<User> users) {
+    _users = users;
+    notifyListeners();
+  }
+
+  void createExpense(Map<String, dynamic> data) {
+    _webSocketService.send({'type': 'createExpense', 'payload': data});
+  }
+
+  void updateExpense(String expenseId, Map<String, dynamic> data) {
+    _webSocketService.send({'type': 'updateExpense', 'payload': data});
+  }
+
+  void deleteExpense(String expenseId) {
+    _webSocketService.send({'type': 'deleteExpense', 'payload': expenseId});
+  }
+
+  void dispose() {
+    _webSocketService.dispose();
+    super.dispose();
+  }
+}
--- lib/widgets/event_expenses_tab.dart
+++ lib/widgets/event_expenses_tab.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:nsm/services/event_websocket_service.dart';
+import 'package:provider/provider.dart';
 
 class EventExpensesTab extends StatelessWidget {
-  const EventExpensesTab({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Dépenses Content'),
+    return Consumer<EventWebsocketProvider>(
+      builder: (context, eventProvider, child) {
+        final expenses = eventProvider.expenses;
+        if (expenses.isEmpty) {
+          return const Center(child: Text('No expenses available'));
+        }
+        return ListView.builder(
+          itemCount: expenses.length,
+          itemBuilder: (context, index) {
+            final expense = expenses[index];
+            return ListTile(
+              title: Text(expense.description),
+              subtitle: Text('${expense.amount} €'),
+            );
+          },
+        );
+      },
     );
   }
 }
