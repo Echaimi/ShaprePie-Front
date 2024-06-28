a55df51a1eec604aadd1588bca96315424668591
tlugat <teo.lugat@ausha.co>
2024-06-28 17:27:17 +0200

    ✨ Dialog

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -3,13 +3,13 @@ import 'package:flutter/cupertino.dart';
 import 'package:nsm/providers/auth_provider.dart';
 import 'package:nsm/services/event_websocket_service.dart';
 import 'package:nsm/widgets/AddButton.dart';
+import 'package:nsm/widgets/dialog.dart';
 import 'package:nsm/widgets/bottom_modal.dart';
 import 'package:nsm/widgets/event_balances_tab.dart';
 import 'package:nsm/widgets/event_expenses_tab.dart';
 import 'package:nsm/widgets/event_invitation_modal.dart';
 import 'package:nsm/widgets/event_users_tab.dart';
 import 'package:nsm/widgets/expense_modal_content.dart';
-import 'package:nsm/widgets/join_event_modal_content.dart';
 import 'package:provider/provider.dart';
 import '../services/websocket_service.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -47,8 +47,12 @@ class _EventScreenState extends State<EventScreen> {
     setState(() {}); // Call to rebuild the widget after initialization
   }
 
-  void deleteEvent() {
+  void _deleteEvent() {
     // TODO: Handle event deletion
+  }
+
+  void _archiveEvent() {
+    // TODO: Handle event archiving
   }
 
   void _showAddOptions(BuildContext context) {
@@ -144,25 +148,32 @@ class _EventScreenState extends State<EventScreen> {
               IconButton(
                 icon: const Icon(Icons.delete, color: Colors.white),
                 onPressed: () {
-                  showDialog(
+                  showCupertinoModalPopup(
                     context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Delete event'),
-                        content: const Text(
-                            'Are you sure you want to delete this event?'),
-                        actions: [
-                          TextButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: const Text('Cancel'),
-                          ),
-                          TextButton(
-                            onPressed: deleteEvent,
-                            child: const Text('Delete'),
-                          ),
-                        ],
-                      );
-                    },
+                    builder: (BuildContext context) => CupertinoActionSheet(
+                      actions: <CupertinoActionSheetAction>[
+                        CupertinoActionSheetAction(
+                          onPressed: () {
+                            _deleteEvent();
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Supprimer l\'évènement'),
+                        ),
+                        CupertinoActionSheetAction(
+                          onPressed: () {
+                            _archiveEvent();
+                            Navigator.pop(context);
+                          },
+                          child: const Text('Archiver l\'évènement'),
+                        ),
+                      ],
+                      cancelButton: CupertinoActionSheetAction(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('Annuler'),
+                      ),
+                    ),
                   );
                 },
               ),
@@ -175,6 +186,8 @@ class _EventScreenState extends State<EventScreen> {
               final event = eventProvider.event;
               final users = eventProvider.users;
               final totalExpenses = eventProvider.totalExpenses;
+              final usersCount = users.length;
+              final expensesCount = eventProvider.expenses.length;
 
               if (event == null) {
                 return const Center(child: CircularProgressIndicator());
@@ -221,47 +234,40 @@ class _EventScreenState extends State<EventScreen> {
                               style: theme.textTheme.titleMedium,
                             ),
                             Text(
-                              'au total pour ${users.length} personnes',
+                              'au total pour $usersCount personnes',
                               style: theme.textTheme.bodyMedium,
                             ),
                           ],
                         ),
                       ),
                     ),
-                    const SizedBox(height: 32.0),
-                    Container(
-                      padding: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        color: Theme.of(context).colorScheme.primaryContainer,
-                      ),
-                      child: TabBar(
-                        indicator:
-                            const BoxDecoration(), // No indicator decoration
-                        labelColor: Theme.of(context)
-                            .colorScheme
-                            .secondary, // Active tab color
-                        unselectedLabelColor:
-                            Colors.white, // Inactive tab color
-                        labelStyle: const TextStyle(
-                            fontSize: 14.0), // Smaller text size
-                        tabs: const [
-                          Tab(
-                            icon: Icon(Icons.person),
-                            text: 'Personnes',
-                          ),
-                          Tab(
-                            icon: Icon(Icons.attach_money),
-                            text: 'Dépenses',
-                          ),
-                          Tab(
-                            icon: Icon(Icons.balance),
-                            text: 'Équilibre',
-                          ),
-                        ],
-                      ),
-                    ),
                     const SizedBox(height: 16.0),
+                    TabBar(
+                      dividerColor:
+                          Theme.of(context).colorScheme.primaryContainer,
+                      indicatorColor: Theme.of(context).colorScheme.secondary,
+                      labelColor: Theme.of(context)
+                          .colorScheme
+                          .secondary, // Active tab color
+                      unselectedLabelColor: Colors.white, // Inactive tab color
+                      labelStyle:
+                          const TextStyle(fontSize: 12.0), // Smaller text size
+                      tabs: [
+                        Tab(
+                          icon: const Icon(Icons.person),
+                          text: 'Pers. ($usersCount)',
+                        ),
+                        Tab(
+                          icon: const Icon(Icons.attach_money),
+                          text: 'Dépenses ($expensesCount)',
+                        ),
+                        const Tab(
+                          icon: Icon(Icons.balance),
+                          text: 'Équilibre',
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24.0),
                     Expanded(
                       child: TabBarView(
                         children: [
