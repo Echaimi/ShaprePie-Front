069db8e8d6f5fe59b6d9e6a73fa37fb60e80b79a
tlugat <teo.lugat@ausha.co>
2024-07-02 21:33:14 +0200

    ✨ Event balances tab

--- lib/widgets/event_balances_tab.dart
+++ lib/widgets/event_balances_tab.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:nsm/services/event_websocket_service.dart';
+import 'package:nsm/widgets/balances_chart.dart';
+import 'package:nsm/widgets/transaction_card.dart';
 import 'package:provider/provider.dart';
 
 class EventBalanceTab extends StatelessWidget {
+  const EventBalanceTab({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Consumer<EventWebsocketProvider>(
       builder: (context, eventProvider, child) {
-        final expenses = eventProvider.expenses;
-        if (expenses.isEmpty) {
-          return const Center(child: Text('No expenses available'));
+        final balances = eventProvider.balances;
+        final transactions = eventProvider.transactions;
+
+        if (balances.isEmpty && transactions.isEmpty) {
+          return const Center(
+              child: Text('No balances or transactions available'));
         }
-        final totalExpenses =
-            expenses.fold(0.0, (sum, expense) => sum + expense.amount);
-        return Center(
-          child: Text('Total Spent: $totalExpenses €'),
+
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  '<-- Fais défiler pour voir tout le monde -->',
+                  style: Theme.of(context).textTheme.bodySmall,
+                ),
+              ),
+              const SizedBox(height: 16),
+              BalancesChart(balances: balances),
+              const SizedBox(height: 32),
+              Text(
+                'Qui raque ?',
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              const SizedBox(height: 16),
+              Column(
+                children: transactions.map((transaction) {
+                  return Column(
+                    children: [
+                      TransactionCard(transaction: transaction),
+                      const SizedBox(height: 16),
+                    ],
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
         );
       },
     );
