e99d452252b6eb7b4e8b1bae646ac119e24f40d0
Echaimi <echaimi@myges.fr>
2024-07-18 10:38:41 +0200

    fix style and fix expense_form

--- lib/services/event_service.dart
+++ lib/services/event_service.dart
@@ -12,6 +12,7 @@ class EventService {
 
   Future<List<Event>> getEvents() async {
     final response = await apiService.get('/events');
+    print('GET /events response: ${response.body}');
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
@@ -24,6 +25,7 @@ class EventService {
 
   Future<Event> createEvent(Map<String, dynamic> data) async {
     final response = await apiService.post('/events', data);
+    print('POST /events response: ${response.body}');
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> responseData = json.decode(response.body);
@@ -36,6 +38,7 @@ class EventService {
 
   Future<Event> getEvent(int eventId) async {
     final response = await apiService.get('/events/$eventId');
+    print('GET /events/$eventId response: ${response.body}');
 
     if (response.statusCode == 200) {
       Map<String, dynamic> data = json.decode(response.body);
@@ -48,6 +51,7 @@ class EventService {
 
   Future<List<User>> getEventUsers(int eventId) async {
     final response = await apiService.get('/events/$eventId/users');
+    print('GET /events/$eventId/users response: ${response.body}');
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
@@ -60,6 +64,8 @@ class EventService {
 
   Future<List<Expense>> getEventExpenses(int eventId) async {
     final response = await apiService.get('/events/$eventId/expenses');
+    print('GET /events/$eventId/expenses response: ${response.body}');
+
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final List expenses = data['data'];
@@ -71,6 +77,7 @@ class EventService {
 
   Future<Event> updateEvent(int eventId, Map<String, dynamic> data) async {
     final response = await apiService.patch('/events/$eventId', data);
+    print('PATCH /events/$eventId response: ${response.body}');
 
     if (response.statusCode == 200) {
       Map<String, dynamic> updatedData = json.decode(response.body);
@@ -82,6 +89,7 @@ class EventService {
 
   Future<void> deleteEvent(int eventId) async {
     final response = await apiService.delete('/events/$eventId');
+    print('DELETE /events/$eventId response: ${response.body}');
 
     if (response.statusCode != 200) {
       throw Exception('Failed to delete event');
@@ -93,19 +101,20 @@ class EventService {
       '/events/join',
       {'code': code},
     );
+    print('POST /events/join response: ${response.body}');
 
     if (response.statusCode == 200) {
       return response.body;
-    } else if (response.statusCode == 409) {
-      throw Exception("User is already in the event");
     } else {
-      throw Exception('Failed to join event');
+      final responseData = json.decode(response.body);
+      throw Exception(responseData['error']);
     }
   }
 
   Future<void> updateEventState(int eventId, String state) async {
     final response =
         await apiService.patch('/events/$eventId/state', {'state': state});
+    print('PATCH /events/$eventId/state response: ${response.body}');
 
     if (response.statusCode != 200) {
       throw Exception('Failed to update event state');
